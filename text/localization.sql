6cd4c55b7b3bee51ce11cd8fd7e5df2953d0a28f
Tim Padjen <tpadjen@gmail.com>
2025-03-05 14:32:24 -0800

    Add localization

--- text/localization.sql
+++ text/localization.sql
@@ -1,20 +1,32 @@
-
 --========================================================================================================================
 --========================================================================================================================
         INSERT OR REPLACE INTO LocalizedText
 			(Tag,	Language,	Text)
 		VALUES
-			('LOC_UI_RESOURCE_ALLOCATION_SHOW_CITIES',		'en_US',	"Show Cities");
-			-- ('LOC_UI_RESOURCE_ALLOCATION_SHOW_CITIES',		'de_DE',	""),
-			-- ('LOC_UI_RESOURCE_ALLOCATION_SHOW_CITIES',		'es_ES',	""),
-			-- ('LOC_UI_RESOURCE_ALLOCATION_SHOW_CITIES',		'fr_FR',	""),
-			-- ('LOC_UI_RESOURCE_ALLOCATION_SHOW_CITIES',		'it_IT',	""),
-			-- ('LOC_UI_RESOURCE_ALLOCATION_SHOW_CITIES',		'ja_JP',	""),
-			-- ('LOC_UI_RESOURCE_ALLOCATION_SHOW_CITIES',		'ko_KR',	""),
-			-- ('LOC_UI_RESOURCE_ALLOCATION_SHOW_CITIES',		'pl_PL',	""),
-			-- ('LOC_UI_RESOURCE_ALLOCATION_SHOW_CITIES',		'pt_BR',	""),
-			-- ('LOC_UI_RESOURCE_ALLOCATION_SHOW_CITIES',		'ru_RU',	""),
-			-- ('LOC_UI_RESOURCE_ALLOCATION_SHOW_CITIES',		'zh_Hans_CN',	""),
-			-- ('LOC_UI_RESOURCE_ALLOCATION_SHOW_CITIES',		'zh_Hant_HK',	""),
+			('LOC_UI_RESOURCE_ALLOCATION_SHOW_CITIES',		'en_US',	"Show Cities"),
+			('LOC_UI_RESOURCE_ALLOCATION_SHOW_CITIES',		'de_DE',	"Städte Anzeigen"),
+			('LOC_UI_RESOURCE_ALLOCATION_SHOW_CITIES',		'es_ES',	"Mostrar Ciudades"),
+			('LOC_UI_RESOURCE_ALLOCATION_SHOW_CITIES',		'fr_FR',	"Afficher Les Villes"),
+			('LOC_UI_RESOURCE_ALLOCATION_SHOW_CITIES',		'it_IT',	"Mostra Città"),
+			('LOC_UI_RESOURCE_ALLOCATION_SHOW_CITIES',		'ja_JP',	"都市を表示"),
+			('LOC_UI_RESOURCE_ALLOCATION_SHOW_CITIES',		'ko_KR',	"도시 표시"),
+			('LOC_UI_RESOURCE_ALLOCATION_SHOW_CITIES',		'pl_PL',	"Pokaż Miasta"),
+			('LOC_UI_RESOURCE_ALLOCATION_SHOW_CITIES',		'pt_BR',	"Mostrar Cidades"),
+			('LOC_UI_RESOURCE_ALLOCATION_SHOW_CITIES',		'ru_RU',	"Показать Города"),
+			('LOC_UI_RESOURCE_ALLOCATION_SHOW_CITIES',		'zh_Hans_CN',	"显示城市"),
+			('LOC_UI_RESOURCE_ALLOCATION_SHOW_CITIES',		'zh_Hant_HK',	"顯示城市"),
+
+			('LOC_UI_RESOURCE_ALLOCATION_UNASSIGN_ALL',		'en_US',	"Unassign All Resources"),
+			('LOC_UI_RESOURCE_ALLOCATION_UNASSIGN_ALL',		'de_DE',	"Alle Ressourcen Freigeben"),
+			('LOC_UI_RESOURCE_ALLOCATION_UNASSIGN_ALL',		'es_ES',	"Desasignar Todos Los Recursos"),
+			('LOC_UI_RESOURCE_ALLOCATION_UNASSIGN_ALL',		'fr_FR',	"Désaffecter Toutes Les Ressources"),
+			('LOC_UI_RESOURCE_ALLOCATION_UNASSIGN_ALL',		'it_IT',	"Annulla Assegnazione Di Tutte Le Risorse"),
+			('LOC_UI_RESOURCE_ALLOCATION_UNASSIGN_ALL',		'ja_JP',	"すべての資源を解除"),
+			('LOC_UI_RESOURCE_ALLOCATION_UNASSIGN_ALL',		'ko_KR',	"모든 자원 할당 해제"),
+			('LOC_UI_RESOURCE_ALLOCATION_UNASSIGN_ALL',		'pl_PL',	"Cofnij Przydział Wszystkich Zasobów"),
+			('LOC_UI_RESOURCE_ALLOCATION_UNASSIGN_ALL',		'pt_BR',	"Desatribuir Todos Os Recursos"),
+			('LOC_UI_RESOURCE_ALLOCATION_UNASSIGN_ALL',		'ru_RU',	"Отменить Назначение Всех Ресурсов"),
+			('LOC_UI_RESOURCE_ALLOCATION_UNASSIGN_ALL',		'zh_Hans_CN',	"取消分配所有资源"),
+			('LOC_UI_RESOURCE_ALLOCATION_UNASSIGN_ALL',		'zh_Hant_HK',	"取消分配所有資源");
 --========================================================================================================================
 --========================================================================================================================
